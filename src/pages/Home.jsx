67d4cbefb775d936b84824f23d4114aab10305d0
sumit <chavansumit121@gmail.com>
2023-08-30 10:31:00 +0530

    addbutton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import WithUser from "../component/WithUser";
-function Home({ user }) {
+
+function Home({ user, logOut }) {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div>
       <div>
         <h1>This is home page{user.email}</h1>
+        <div>
+          <button
+            onClick={handleLogout}
+            className="bg-black text-white w-36 rounded-sm"
+          >
+            logout
+          </button>
+        </div>
       </div>
     </div>
   );
